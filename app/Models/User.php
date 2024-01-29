2f445f172c388e4701a5152e4c22b02946ad9a6c
Everton Muniz <munizeverton@gmail.com>
2024-01-29 16:59:48 -0300

    Changes users table to work with uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUuids, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +45,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected $keyType = 'string';
+
+    protected static function booted(): void
+    {
+        static::creating(static fn(User $user) => $user->id = (string)Str::uuid());
+    }
 }
